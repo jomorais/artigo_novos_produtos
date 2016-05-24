3362222589247140004adf17e70d588a9415797e
nilson <nilsonmont.o@gmail.com>
2016-05-24 15:58:12 -0400

    Novas alterações..

---
+++ Artigo_IEEE/config_cam.py
@@ -0,0 +1,9 @@
+
+# auto-exposicao desligada
+"v4l2-ctl -c auto_exposure = 1"
+# balanco de branco manual
+"v4l2-ctl -c white_balance_auto_preset = 0"
+# desabilita deteccao de freq. de linha
+"v4l2-ctl -c power_line_frequency = 0" 
+# decremento na saturacao (Cor)
+"v4l2-ctl -c saturation = -30" 
